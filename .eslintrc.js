31e6aed947efccc55e28a474ec8b8c5a922eeb63
END-i <evgeniy.donev@gmail.com>
2023-03-06 12:22:53 +0200

    config fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -13,13 +18,19 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    indent: ['error', 2],
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+        ignoredNodes: ['ConditionalExpression'],
+      },
+    ],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     // 'prettier/prettier': ['error'],
     // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': ['error'],
   },
 };
